1155c5172b55e3435e9e160bd9d1451a187975f3
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-02 12:07:57 -0500

    added junit

--- src/main/java/MavenExercise.java
+++ src/main/java/MavenExercise.java
@@ -1,16 +1,16 @@
 import org.apache.commons.lang3.StringUtils;
-
 import java.util.Scanner;
 
 public class MavenExercise {
     public static void main(String[] args) {
+        //https://commons.apache.org/proper/commons-lang/apidocs/org/apache/commons/lang3/StringUtils.html
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Something:");
         String userInput = scanner.nextLine();
         System.out.printf("You Entered: %s%n", userInput);
         System.out.println(StringUtils.isNumeric(userInput) ? userInput + " is a number" : userInput + " is not a number");
-        StringUtils string = new StringUtils();
-        System.out.printf("Flipped Case: %s",string.swapCase(userInput));
-        System.out.printf("Reversed: %s",string.reverse(userInput));
+//        StringUtils string = new StringUtils();
+        System.out.printf("Flipped Case: %s",StringUtils.swapCase(userInput));
+        System.out.printf("Reversed: %s",StringUtils.reverse(userInput));
     }
 }
