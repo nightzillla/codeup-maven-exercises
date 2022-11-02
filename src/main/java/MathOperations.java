7499808ab2c9c44a5d09278230ce18f7e7646086
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-02 16:56:01 -0500

    added files

--- src/main/java/MathOperations.java
+++ src/main/java/MathOperations.java
@@ -3,4 +3,28 @@ public class MathOperations {
     public static long add(long input1, long input2){
         return input1 + input2;
     }
-}
+
+    public static double tip(double bill, double tip){
+        return bill * (tip/100);
+    }
+
+    public static int[] addOneToArray(int[] numbers) {
+        int i = 0;
+        for(int number : numbers){
+            numbers[i++] = number + 1;
+        }
+        return numbers;
+    }
+//        for (int i = 0; i < numbers.length; i++) {
+//            numbers[i] += 1;
+//        }
+//        return numbers;
+//    }
+
+
+
+    public static void main(String[] args) {
+        int[] startArray = {1,2,3,4,5};
+
+    }
+} // end of MathOperations
--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,7 +1,27 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class MyFirstTest {
+
+    ArrayList<Integer> arrayList = new ArrayList<>();
+    ArrayList<Integer> arrayList2 = new ArrayList<>();
+
+    @Before
+    public void setUpValues(){
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList2.add(1);
+        arrayList2.add(12);
+    }
+
+    @Test
+    public void testObjectSameness(){
+        assertSame(arrayList, arrayList2);
+    }
 
     @Test
     public void additionMethodTest(){
@@ -11,7 +31,21 @@ public class MyFirstTest {
         assertEquals(expected, MathOperations.add(input1, input2));
     }
 
+    @Test
+    public void tipAppTest(){
+        double cost = 22.56;
+        double tip = 20;
 
+        assertEquals(4.51, MathOperations.tip(cost, tip),0.05);
 
+    }
+
+    @Test
+    public void testArrayEquality(){
+        int[] startArray = {1,2,3,4,5};
+        int[] endArray = {2,3,4,5,6};
+
+        assertArrayEquals(endArray, MathOperations.addOneToArray(startArray));
+    }
 
 }
