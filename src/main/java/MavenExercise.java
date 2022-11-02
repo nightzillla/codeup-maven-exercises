66d324ade1fef4d13b7281d823863eae139997d2
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-02 11:29:02 -0500

    added ternary operator

--- src/main/java/MavenExercise.java
+++ src/main/java/MavenExercise.java
@@ -7,9 +7,10 @@ public class MavenExercise {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Something:");
         String userInput = scanner.nextLine();
-        System.out.printf("You Entered: %s is not a number%n", userInput);
+        System.out.printf("You Entered: %s%n", userInput);
+        System.out.println(StringUtils.isNumeric(userInput) ? userInput + " is a number" : userInput + " is not a number");
         StringUtils string = new StringUtils();
-        System.out.printf("Flipped Case: %s%n",string.swapCase(userInput));
+        System.out.printf("Flipped Case: %s",string.swapCase(userInput));
         System.out.printf("Reversed: %s",string.reverse(userInput));
     }
 }
